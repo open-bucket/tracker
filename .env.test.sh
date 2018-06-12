f2cd1e9539cf5c96799cf7f0553435b978e86165
Hung Vu <vunguyenhung@outlook.com>
2018-06-12 19:42:18 +0700

    - Add validate rules to POST /consumers/:id

--- .env.test.sh
+++ .env.test.sh
@@ -4,5 +4,5 @@ export DB_DIALECT='postgres'
 export DB_HOST='localhost'
 export DB_PORT=5432
 export DB_NAME='obn-test'
-export DB_LOGGING=1
+export DB_LOGGING=0
 export DEBUG='obn-tracker:*'
--- src/routes/consumers.js
+++ src/routes/consumers.js
@@ -16,15 +16,19 @@ const {create, getConsumerByIdAndUserId} = require('../services/consumer');
 const log = createLogFn('routes:consumer');
 
 router
-    .get('/:id', auth(), (request, response, next) => {
-        const {id} = request.params;
-        const userId = request.user.id;
-        return getConsumerByIdAndUserId({id, userId})
-            .then(consumer => consumer
-                ? response.send(consumer)
-                : response.status(NOT_FOUND).send(`Can't find your consumer with id: ${request.params.id}`))
-            .catch(next);
-    })
+    .get('/:id',
+        validate([check('id')
+            .not().isEmpty()
+            .isNumeric()]),
+        auth(), (request, response, next) => {
+            const {id} = request.params;
+            const userId = request.user.id;
+            return getConsumerByIdAndUserId({id, userId})
+                .then(consumer => consumer
+                    ? response.send(consumer)
+                    : response.status(NOT_FOUND).send(`Can't find your consumer with id: ${request.params.id}`))
+                .catch(next);
+        })
     .post('/',
         validate([check('address').trim().not().isEmpty()]),
         auth(),
@@ -32,10 +36,7 @@ router
             const {address} = request.body;
             const userId = request.user.id;
             return create({address, userId})
-                .then((data) => {
-                    log('inside post consumers:', data);
-                    return response.status(CREATED).send(data);
-                })
+                .then((data) => response.status(CREATED).send(data))
                 .catch(next);
         });
 
